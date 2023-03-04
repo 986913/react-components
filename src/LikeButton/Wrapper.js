0273aa03caef46f460d7a8d5092c6ff42dc29505
Sun flower <MLIU2@LTU.EDU>
2023-03-04 14:54:14 -0500

    remove unnecessary

--- src/LikeButton/Wrapper.js
+++ src/LikeButton/Wrapper.js
@@ -2,9 +2,5 @@ import React from 'react';
 import { LikeButton } from './LikeButton';
 
 export const LikeBtnWrapper = () => {
-  return (
-    <div>
-      <LikeButton />
-    </div>
-  );
+  return <LikeButton />;
 };
