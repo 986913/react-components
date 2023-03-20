ca72ac2e77e61ccb896610e5aff18d6ccf67cc95
Sun flower <MLIU2@LTU.EDU>
2023-03-20 12:19:29 -0400

    comments added

--- src/TodoListII/Wrapper.js
+++ src/TodoListII/Wrapper.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TodoList } from './TodoList';
 
+/* This is pure redux version of todolist */
 export const TodolistWrapperII = () => {
   return <TodoList />;
 };
