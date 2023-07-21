633dc4cfb39b63d6f8c0c5140bcbb52681f6ab8c
Sun flower <MLIU2@LTU.EDU>
2023-07-21 16:28:46 -0400

    quiz

--- React_Quizzes/19_lazy initial state.js
+++ React_Quizzes/19_lazy initial state.js
@@ -28,3 +28,19 @@ ReactDOM.render(<App />, document.getElementById('root'));
 1
 3
  */
+
+/**
+  
+  why is it not 2 1 2 3?
+    -  function in useState it's a lazy initialization, it will be executed only on the initial render https://www.shauryakalia.com/blogs/react-js-performance-use-state-lazy-initialization
+
+
+  If you pass function to useState, then that function gets called right away.  
+    const [state2] = useState(() => {
+      console.log('inside hook', 2); // One console statement
+      return 2;
+    });
+  This will print inside hook 2 right away
+  Then component will try to render which will print state 1 i.e. 2
+  Once component is rendered, useEffect will be called this will call setState1. Updating state will cause component to render again and print Console from main 3
+ */
