30b58ecf4b74d6041b78f2db774deccd06a096ee
Sun flower <MLIU2@LTU.EDU>
2023-10-30 14:20:31 -0400

    remove unuseful stuff

--- src/Hooks/CustomizeHooks/useUpdateEffect/App.js
+++ src/Hooks/CustomizeHooks/useUpdateEffect/App.js
@@ -1,5 +1,5 @@
 import { useUpdateEffect } from './useUpdateEffect';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function App() {
   /*********************** Hooks usages *************************/
--- src/Hooks/CustomizeHooks/useUpdateEffect/useUpdateEffect.js
+++ src/Hooks/CustomizeHooks/useUpdateEffect/useUpdateEffect.js
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 /*
   Implement useUpdateEffect() that it works the same as useEffect() 
